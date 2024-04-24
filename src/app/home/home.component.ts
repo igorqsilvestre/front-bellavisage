52983c99e8300ab6d83bd9dc5bc3d1820f31dfc1
Igor Queiroz <igorczz.10@gmail.com>
2024-04-24 18:04:51 -0300

    subindo projeto pela primeira vez

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+  nomeCards: string[] = ['Agendar Atendimento', 'Gerenciar Agendamentos', 'Gerenciar Pagamentos', 'Gerenciar Especialista', 'Gerenciar Clínica',
+    'Faturamento', 'Gerenciar Tratamentos', 'Agendar Atendimento', 'Avalie o serviço'
+  ];
+}
